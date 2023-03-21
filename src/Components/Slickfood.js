f80d588151184fd944c3d3464749e00411626e72
TheTRz112 <thetrz112@gmail.com>
2023-03-22 02:07:25 +0700

    tour

--- src/Components/Slickfood.js
+++ src/Components/Slickfood.js
@@ -1,35 +1,29 @@
-
 // import React, { settings } from 'react';
-import Slider from "react-slick";
-import pic1 from "images/img1.png";
-import pic2 from "images/img2.png";
-import pic3 from "images/img3.png";
-function Slickfood(){
+import Slider from 'react-slick';
+import React from 'react';
+function Slickfood(props) {
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
-    slidesToScroll: 1
+    slidesToScroll: 1,
   };
 
-return(
-  <div className="reactSlick">
-    <Slider {...settings}>
-      <div>
-        <img src={pic1}/>
-      </div>
-      <div>
-      <img src={pic2}/>
-      </div>
-      <div>
-      <img src={pic3}/>
-      </div>
-    </Slider>
-  </div>
-);
+  return (
+    <div className="reactSlick">
+      <Slider {...settings}>
+        <div>
+          <img src="images/img1.png" alt="" />
+        </div>
+        <div>
+          <img src="images/img2.png" alt="" />
+        </div>
+        <div>
+          <img src="images/img3.png" alt="" />
+        </div>
+      </Slider>
+    </div>
+  );
 }
 export default Slickfood;
-
-
-
