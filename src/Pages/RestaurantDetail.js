0531fe549f3a86ddba9593bfbde3e2c4214b3f27
NontapatB <b.nontapat.work@gmail.com>
2023-04-13 21:16:41 +0700

    Seeallpage/pass id to restaurantdetail page

--- src/Pages/RestaurantDetail.js
+++ src/Pages/RestaurantDetail.js
@@ -1,7 +1,18 @@
 import React from 'react';
+// import { restaurant } from '../data.js';
+// import { FaStar } from 'react-icons/fa';
+import { useParams } from 'react-router-dom';
+import { restaurant } from '../data';
 
 const RestaurantDetail = () => {
-  return <div>RestaurantDetail</div>;
-};
+  const { id } = useParams();
+  const selectedRestaurant = restaurant.find((item) => item.id === parseInt(id));
+  console.log(selectedRestaurant);
+  return(
+    <div>
+      RestaurantDetail
+    </div>
+  );
+}
 
 export default RestaurantDetail;
--- src/Pages/SeeAll.js
+++ src/Pages/SeeAll.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import { restaurant } from '../data.js';
+import '../Styles/HeadlineCards.css';
+import '../Styles/Home.css';
+import { FaStar } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const SeeAll = () => {
   return (
-  <div>
-    <h1>Seeall</h1>
-    </div>
+    <div className="top3-con">
+        {restaurant.map((values) => {
+          const { id, title, desc, rating, review, img } = values;
+          return (
+            <Link to={`/detail/${id}`}>
+              <div  class="card" key={id}>
+                <img src={img} alt="/" />
+                <div class="textborder">
+                  <p class="headp">{title}</p>
+                  <p class="subp">{desc}</p>
+                  <div style={{ display: 'flex' }}>
+                    <p id="textstar">
+                      {rating} <FaStar />
+                    </p>
+                    <p id="textreview">{review} review</p>
+                  </div>
+                </div>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
   )
 };
 
