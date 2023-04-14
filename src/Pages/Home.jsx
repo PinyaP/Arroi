9063502ce9bf01dae8fbbc76f1b71f0959030824
jerryctk <jjerryctk@gmail.com>
2023-04-14 19:15:21 +0700

    edit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,10 +5,12 @@ import '../Styles/HeadlineCards.css';
 import Slickfood from '../Components/Slickfood';
 import { restaurant } from '../data.js';
 import { FaStar } from 'react-icons/fa';
+import { BsFire } from 'react-icons/bs';
 
 const Home = () => {
   const [data, setData] = useState(restaurant);
   const [showLastButton, setShowLastButton] = useState(false);
+  const [showRank, setShowRank] = useState(false);
   
   const filterResult = (catItem) => {
     const result = restaurant.filter((curData) =>
@@ -18,8 +20,10 @@ const Home = () => {
     
     if (catItem === 'a' || catItem === 'newres') {
       setShowLastButton(true);
+      setShowRank(false);
     } else {
       setShowLastButton(false);
+      setShowRank(true);
     }
   };
 
@@ -40,6 +44,13 @@ const Home = () => {
           </button>
         </ul>
       </div>
+
+      <div className='con-rank'>
+        {showRank && <p class='rank2'>NO.2 <BsFire /></p>}
+        {showRank && <p class='rank1'>NO.1 <BsFire/> </p>}
+        {showRank && <p class='rank3'>NO.3 <BsFire/> </p>}
+      </div>
+      
       <div className="top3-con">
         {data.map((values) => {
           const { id, title, desc, rating, review, img } = values;
@@ -59,9 +70,13 @@ const Home = () => {
             </Link>
           );
         })}
+        <div className='con-button'>
+        {showLastButton && <button id="seeallbtn"><span>See All </span></button>}
+        </div>
       </div>
-      {showLastButton && <button id="seeallbtn"><span>Hover </span></button>}
+      
     </div>
+    
   );
 };
 
